e51fbcb93a1f88c80b4f8758be0dd7783b806e18
Thomas Gander - School <thomas.gander@mymhc.ca>
2023-10-18 10:55:47 -0600

    Add project files.

---
+++ WPFDemo1/ProductRecords.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFDemo1
+{
+    public class ProductRecords
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public int SupplierID { get; set; }
+
+    }
+}
